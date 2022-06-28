30483d5e77ef19566246ff625076356f34e180cf
Vincent Tate <vincentjtate@gmail.com>
2022-06-27 22:54:05 -0700

    fixing an issue of making sure there is at least one char if you select yes on the option

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function writePassword() {
 
   while(answer == false){
   
-    passwordCOMPLEX.totalLENGTH = window.prompt("How long would you want the password?");
+    passwordCOMPLEX.totalLENGTH = window.prompt("How long would you want the password? LIMIT: 8 - 128 Characters");
     passwordCOMPLEX.lowerCASE = window.confirm("Do you want lower case letters?");
     passwordCOMPLEX.upperCASE = window.confirm("Do you want upper case letters?");
     passwordCOMPLEX.hasNUM = window.confirm("Do you want numbers?");
@@ -61,74 +61,148 @@ function passwordCHECKER(checkMATE)
 
 function generatePassword(passBOX){
   var endWORK = [];
+  var lastRUN = false;
+
 
   //probably a dumb way of generating a password, but this is what made sense to me.
-  for (var i = 0; i <= passBOX.totalLENGTH; i++)
+  while (lastRUN == false)
   {
-    var goodRUN = false;
-    var insertCHAR;
-    var randomOUTPUT = Math.floor(Math.random() * 4);
-    switch (randomOUTPUT){
-      case 0:
-        if(passBOX.lowerCASE == true)
-        {
-            insertCHAR = generateLOWER();
-            goodRUN = true;
-        }
-        else
-        {
-          //negative increments are to keep the char placement in the same spot of the array so that no spots are missed.
-          i--;
-        }
-        break;
-      case 1:
-        if(passBOX.upperCASE == true)
-        {
-          insertCHAR = generateUPPER();
-          goodRUN = true;
-        }
-        else
-        {
-          i--;
-        }
-        break;
-      case 2:
-        if(passBOX.hasNUM == true)
-        {
-          insertCHAR = Math.floor(Math.random()*10);
-          goodRUN = true;
-        }
-        else
-        {
-          i--;
-        }
-        break;
-      case 3:
-        if(passBOX.hasSPECIAL == true)
-        {
-          insertCHAR = generateSpecial();
-          goodRUN = true;
-        }
-        else
-        {
-          i--;
-        }
-        break;
-      default:
-        break;
-    }
-    //If the run is 'good' then this action will splice the char into the array.
-    if (goodRUN == true)
+    var lowerCOUNT = 0;
+    var upperCOUNT = 0;
+    var numCOUNT = 0;
+    var specialCOUNT = 0;
+
+
+    for (var i = 0; i <= passBOX.totalLENGTH-1; i++)
     {
-      endWORK.splice(i,0, insertCHAR);
+      var goodRUN = false;
+      var insertCHAR;
+      var randomOUTPUT = Math.floor(Math.random() * 4);
+
+      switch (randomOUTPUT){
+        case 0:
+          if(passBOX.lowerCASE == true)
+          {
+              insertCHAR = generateLOWER();
+              lowerCOUNT++
+              goodRUN = true;
+          }
+          else
+          {
+            //negative increments are to keep the char placement in the same spot of the array so that no spots are missed.
+            i--;
+          }
+          break;
+        case 1:
+          if(passBOX.upperCASE == true)
+          {
+            insertCHAR = generateUPPER();
+            upperCOUNT++;
+            goodRUN = true;
+          }
+          else
+          {
+            i--;
+          }
+          break;
+        case 2:
+          if(passBOX.hasNUM == true)
+          {
+            insertCHAR = Math.floor(Math.random()*10);
+            numCOUNT++;
+            goodRUN = true;
+          }
+          else
+          {
+            i--;
+          }
+          break;
+        case 3:
+          if(passBOX.hasSPECIAL == true)
+          {
+            insertCHAR = generateSpecial();
+            specialCOUNT++
+            goodRUN = true;
+          }
+          else
+          {
+            i--;
+          }
+          break;
+        default:
+          break;
+      }
+      //If the run is 'good' then this action will splice the char into the array.
+      if (goodRUN == true)
+      {
+        endWORK.splice(i,0, insertCHAR);
+      }
+      console.log("Cycle count", i);
+    }
+    lastRUN = finalCHECKER(passBOX, lowerCOUNT, upperCOUNT, numCOUNT, specialCOUNT);
+    //if the checker returns false then this will clear out the array allowing a new clean password to be generated.
+    if (lastRUN == false)
+    {
+      endWORK = [];
+      console.log("Clearing out endwork: ", endWORK )
     }
   }
+
   var endGame = endWORK.join("");
 
   //There use to be a window alert here that would also showcase the password, but as soon as I used
   // the .join function it pooped itself when special characters were involved. Idk why but it just
   // forced me to push this to text box above the generator button.
   return endGame;
+}
+
+
+// This function checks if there is at least one of each
+function finalCHECKER(finalBOX, lower, upper, num, special)
+{
+  if (finalBOX.lowerCASE == true)
+  {
+    if(lower > 0)
+    {
+    }
+    else
+    {
+      return false
+    }
+  }
+  if (finalBOX.upperCASE == true)
+  {
+    if(upper > 0)
+    {
+    }
+    else
+    {
+      return false
+    }
+  }
+  if (finalBOX.hasNUM == true)
+  {
+    if(num > 0)
+    {
+    }
+    else
+    {
+      return false
+    }
+  }
+  if (finalBOX.hasSPECIAL == true)
+  {
+    if(special > 0)
+    {
+    }
+    else
+    {
+      return false
+    }
+  }
+
+  return true;
+
 }
 
 
